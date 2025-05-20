39f1c0e410aab1272f3829bce367e77d474b03d5
juliac888 <119269739+juliac888@users.noreply.github.com>
2025-05-20 16:39:30 -0700

    Delete Frontend directory
    No longer need this folder. I reorganized the old files in a new Folder that can be run as a React App which was faster than just adding everything required to this old folder.

--- Frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useState } from 'react';
-
-/* 
-    this should eventually piece everything together, like login verification
-    the eventual calandar app code and other stuff. Im assuming we are going to do 
-    calendar stuff in another js file so we can organize and split things up.
-*/
