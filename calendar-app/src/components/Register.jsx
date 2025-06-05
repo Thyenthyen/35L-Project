888945e83c708dbbdc3db09aff321fd376f3151d
Thyenthyen <142359510+Thyenthyen@users.noreply.github.com>
2025-06-04 21:45:09 -0700

    Change page same as login page Register.jsx

--- calendar-app/src/components/Register.jsx
+++ calendar-app/src/components/Register.jsx
@@ -29,15 +29,32 @@ function Register({ onRegister }) {
 
   return (
     <div className="auth-container">
-      <h2>Register</h2>
+    <div className="auth-form">
+      <h2>Register Your Personal Calendar</h2>
       {error && <p className="error">{error}</p>}
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" placeholder="username" value={form.Username} onChange={handleChange} required />
-        <input type="password" name="password" placeholder="password" value={form.Password} onChange={handleChange} required />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={form.email}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={form.password}
+          onChange={handleChange}
+          required
+        />
         <button type="submit">Register</button>
       </form>
+    </div>
     </div>
   );
 }
 
 export default Register;
+
